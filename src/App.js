7ea25c2ed3ab3282f8132512a93694c9861703ec
kovacsmmarci <janikaee@gmail.com>
2025-04-24 22:00:34 +0200

    change in src

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
      <div className="App">
         <h1>🚀 Hello from Dockerized React CI/CD App!</h1>
-	<p>Just a random paragraph and more words</p>
+	<p>Just a random paragraph and more words, and more</p>
      </div>
   );
 }
