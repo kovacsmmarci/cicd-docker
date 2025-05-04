08e23bab1517b1756b381049c5cf2236824d9373
kovacsmmarci <janikaee@gmail.com>
2025-05-04 18:44:20 +0200

    test commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,8 @@ import './App.css';
 function App() {
   return (
      <div className="App">
-        <h1>🚀 Hello from Dockerized React CI/CD App!</h1>
-	<p>Just a random paragraph and more words, and more</p>
-	<h2>Another title</h2>
+        <h1>Hello from a Dockerized React App!</h1>
+	<p>Just a random paragraph.</p>
      </div>
   );
 }
